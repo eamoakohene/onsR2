0828591d5b69cd83197747fb334271f9a08b0f1f
Emmanuel Amoakohene <eamoakohene@gmail.com>
2016-03-07 22:42:22 +0000

    Database update

--- R/onsR2.R
+++ R/onsR2.R
@@ -1,5 +1,8 @@
 
-
+#' Download ONS data using series 4-letter code (eg. cpi=D7BT)
+#' format - output format, ts=timeseries, df=dataframe
+#' fx - the source of data, ts = timeseries data, ds= dataset which is normally large
+#'  spreadsheet containing several other series. It not as effecient as the ts source.
 
 download <- function(code,format = 'ts',fx = 'ts') {
   if(fx == 'ts'){
@@ -16,10 +19,10 @@ download <- function(code,format = 'ts',fx = 'ts') {
 
 
 search <- function(qry = NULL,is_code = FALSE,fx = 'ts') {
-  fxn_show_boat(msg = match.call()[[1]])
+  #fxn_show_boat(msg = match.call()[[1]])
   if(fx == 'ts'){
      my_temp <- onsR2::ts_read$new( code_req = FALSE)
-     fxn_show_boat('SEARCH |> MY_TEMP OK')
+     #fxn_show_boat('SEARCH |> MY_TEMP OK')
       return(
         my_temp$search_info(qry = qry,is_code=is_code)
       )
@@ -33,6 +36,6 @@ search <- function(qry = NULL,is_code = FALSE,fx = 'ts') {
 
 fxn_show_boat <- function(msg = match.call()[[1]]){
 
-  my_msg <- sprintf("Hi I am in %s wheeeeee!",toupper(msg))
+  #my_msg <- sprintf("Hi I am in %s wheeeeeez!",toupper(msg))
   #cat(my_msg,'\n')
 }
--- R/scrap.R
+++ R/scrap.R
@@ -1,9 +1,9 @@
-
-# ts_search_info <- function(code='mgsx', cs = '.btn--primary' ){
-#     my_code <- code
-#     my_css <- cs
 #
-#     my_url <- paste0('http://www.ons.gov.uk/search?q=',tolower(my_code))
+# scrap_info <- function(uri='http://www.ons.gov.uk/search', css = '.btn--primary' ){
+#     my_url <- uri
+#     my_css <- css
+#
+#
 #     my_page <- try(xml2::read_html(my_url),silent = T)
 #
 #     if(class(my_page)[1]=='try-error'){
@@ -14,16 +14,7 @@
 #
 #     my_element <- rvest::html_nodes(my_page, my_css)
 #     if(is.null(my_element)){ return(NULL) }
-#
-#     my_links <- rvest::html_attr(my_element, "href")
-#     if(is.null(my_links)){ return(NULL) }
-#
-#     my_substr_start <- nchar('/generator?format=') + 1
-#     my_substr_end <- my_substr_start +nchar('csv') - 1
-#     my_link_csv <- my_links[ substr( my_links,my_substr_start, my_substr_end ) == 'csv' ]
-#
-#     return(my_link_csv[1])
+#     return(my_element)
 # }
-
-
-#onsR2::add_missing_codes()
+#
+#
